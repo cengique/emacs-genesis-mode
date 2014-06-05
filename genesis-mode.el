ef8a567ce4a324137ea8804269de79375d6c7f58
Cengiz Gunay <cengique@users.sf.net>
2014-06-05 13:45:16 -0400

    better installation instructions and description of providd functionality.

--- genesis-mode.el
+++ genesis-mode.el
@@ -11,6 +11,27 @@
 ;; Copyright (C) 2005-2006 Hugo Cornelis
 ;; Copyright (C) 2000, 2003 Scott Andrew Borton <scott@pp.htv.fi>
 
+;;; Installation:
+
+;; Load this file in (X)Emacs. Placing it under ~/.emacs.d/ should
+;; make it autoload it Emacs. Otherwise place the following lines in
+;; your ~/.emacs file:
+;; 	(add-to-list 'load-path "~/.emacs.d/")
+;; 	(require 'genesis-mode)
+;; In XEmacs, you could place the following in your ~/.xemacs/init.el file:
+;; 	(load-file "~/.xemacs/genesis-mode.el")
+
+;;; Notes:
+;; 
+;; This mode provides:
+;; * syntax highlighting for Genesis keywords, functions, and objects. 
+;; * automatic indentation with the tab key
+;; * function index menu item
+;;
+;; This mode uses an example used in a tutorial about Emacs
+;; mode creation. The tutorial can be found here:
+;; http://renormalist.net/Renormalist/EmacsLanguageModeCreationTutorial
+
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 2 of
@@ -25,12 +46,6 @@
 ;; License along with this program; if not, write to the Free
 ;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 ;; MA 02111-1307 USA
-
-;;; Commentary:
-;; 
-;; This mode uses an example used in a tutorial about Emacs
-;; mode creation. The tutorial can be found here:
-;; http://renormalist.net/Renormalist/EmacsLanguageModeCreationTutorial
 
 ;; TODO:
 ;; - test in Xemacs
